6194b7dd53b1e414b130bcf48e484305e4fb74cc
Rémi Sarrailh <remi@madnerd.org>
2020-10-28 20:49:41 +0100

    Set HTTPS to insecure
    This solution means the ESP8266 will not check if the identity of the server is valid.
    Not the best solution but this will works even if the certificate changes.

--- discord_test_esp8266/discord.h
+++ discord_test_esp8266/discord.h
@@ -1,19 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
-#include <WiFiClientSecureBearSSL.h>
+#include <WiFiClientSecure.h>
+#include "WiFiClientSecureAxTLS.h"
 
 const char ssid[] = SECRET_SSID;    // Network SSID (name)
 const char pass[] = SECRET_PASS;    // Network password (use for WPA, or use as key for WEP)
 const String discord_webhook = SECRET_WEBHOOK;
 const String discord_tts = SECRET_TTS;
-//openssl s_client -showcerts -connect discordapp.com:443 (get last certificate)
-const uint8_t discordappFingerprint[20] = {0x2D, 0x08, 0xE9, 0x2D, 0x0A, 0x54, 0x5A, 0xD5, 0xB4, 0x0A, 0x57, 0xF6, 0x68, 0x85, 0x1A, 0x79, 0x52, 0xE1, 0xFA, 0x65};
 ESP8266WiFiMulti WiFiMulti;
+WiFiClientSecure client;
 
 void connectWIFI() {
   WiFiMulti.addAP(ssid, pass);
   WiFi.mode(WIFI_STA);
+  client.setInsecure();
   Serial.print("[WiFi] Connecting to: ");
   Serial.println(ssid);
   // wait for WiFi connection
@@ -26,45 +27,36 @@ void connectWIFI() {
 }
 
 void sendDiscord(String content) {
-  std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+  HTTPClient https;
+  Serial.println("[HTTP] Connecting to Discord...");
+  Serial.println("[HTTP] Message: " + content);
+  Serial.println("[HTTP] TTS: " + discord_tts);
+  if (https.begin(client, discord_webhook)) {  // HTTPS
+    // start connection and send HTTP header
+    https.addHeader("Content-Type", "application/json");
+    int httpCode = https.POST("{\"content\":\"" + content + "\",\"tts\":" + discord_tts + "}");
 
-  if (client) {
-    client->setFingerprint(discordappFingerprint);
-    {
-      HTTPClient https;
-      Serial.println("[HTTP] Connecting to Discord...");
-      Serial.println("[HTTP] Message: " + content);
-      Serial.println("[HTTP] TTS: " + discord_tts);
-      if (https.begin(*client, discord_webhook)) {  // HTTPS
-        // start connection and send HTTP header
-        https.addHeader("Content-Type", "application/json");
-        int httpCode = https.POST("{\"content\":\"" + content + "\",\"tts\":" + discord_tts +"}");
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.print("[HTTP] Status code: ");
+      Serial.println(httpCode);
 
-        // httpCode will be negative on error
-        if (httpCode > 0) {
-          // HTTP header has been send and Server response header has been handled
-          Serial.print("[HTTP] Status code: ");
-          Serial.println(httpCode);
-
-          // file found at server
-          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-            String payload = https.getString();
-            Serial.print("[HTTP] Response: ");
-            Serial.println(payload);
-          }
-        } else {
-          Serial.print("[HTTP] Post... failed, error: ");
-          Serial.println(https.errorToString(httpCode).c_str());
-        }
-
-        https.end();
-      } else {
-        Serial.printf("[HTTP] Unable to connect\n");
+      // file found at server
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        String payload = https.getString();
+        Serial.print("[HTTP] Response: ");
+        Serial.println(payload);
       }
-
-      // End extra scoping block
+    } else {
+      Serial.print("[HTTP] Post... failed, error: ");
+      Serial.println(https.errorToString(httpCode).c_str());
     }
+
+    https.end();
   } else {
-    Serial.println("[HTTP] Unable to create client");
+    Serial.printf("[HTTP] Unable to connect\n");
   }
+
+  // End extra scoping block
 }
